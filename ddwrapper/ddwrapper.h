04c218e39a1909b9a9abf7dc82c928ffca522f6c
andreaukk <andreaukk@gmx.com>
2018-01-08 08:57:17 +0100

    Now wraps DirectDrawSurface

--- ddwrapper/ddwrapper.h
+++ ddwrapper/ddwrapper.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 /*
 Define data type of hooked functions
 */
@@ -28,33 +27,49 @@ typedef HRESULT(__stdcall* wGetDDSurfaceLocal) (DWORD, DWORD, DWORD);
 typedef HRESULT(__stdcall* wGetSurfaceFromDC_export) (DWORD, DWORD, DWORD);
 typedef HRESULT(__stdcall* wRegisterSpecialCase) (DWORD, DWORD, DWORD, DWORD);
 
-typedef HRESULT (__stdcall* wCompact)(IDirectDraw* ddObj);
-typedef HRESULT (__stdcall* wCreateClipper)(IDirectDraw* ddObj, DWORD dwFlags, LPDIRECTDRAWCLIPPER *lpDDClipper, IUnknown *pUnk);
-typedef HRESULT (__stdcall* wCreatePalette)(IDirectDraw* ddObj, DWORD dwFlags, LPPALETTEENTRY lpDDColor, LPDIRECTDRAWPALETTE *lpDDPalette, IUnknown *pUnk);
-typedef HRESULT (__stdcall* wCreateSurface)(IDirectDraw* ddObj, LPDDSURFACEDESC2 lpDDSurfDesc, LPDIRECTDRAWSURFACE7 *lpDDSurface, IUnknown *pUnk);
-typedef HRESULT (__stdcall* wDuplicateSurface)(IDirectDraw* ddObj, LPDIRECTDRAWSURFACE7 lpDDSurface, LPDIRECTDRAWSURFACE7 *lplpDDSurface);
-typedef HRESULT (__stdcall* wEnumDisplayModes)(IDirectDraw* ddObj, DWORD dwFlags, LPDDSURFACEDESC2 lpDDSD2, LPVOID lpContext, LPDDENUMMODESCALLBACK2 lpEnumModeCallback);
-typedef HRESULT (__stdcall* wEnumSurfaces)(IDirectDraw* ddObj, DWORD dwFlags, LPDDSURFACEDESC2 lpDDSD2, LPVOID lpContext, LPDDENUMSURFACESCALLBACK7 lpEnumSurfCallback);
-typedef HRESULT (__stdcall* wEvaluateMode)(IDirectDraw* ddObj, DWORD dwFlags, DWORD *pSecUntilTimeout);
-typedef HRESULT (__stdcall* wFlipToGDISurface)(IDirectDraw* ddObj);
-typedef HRESULT (__stdcall* wGetAvailableVidMem)(IDirectDraw* ddObj, LPDDSCAPS2 lpDDSCaps, LPDWORD lpdwTotal, LPDWORD lpdwFree);
-typedef HRESULT (__stdcall* wGetCaps)(IDirectDraw* ddObj, LPDDCAPS lpDDDriverCaps, LPDDCAPS lpDDHELCaps);
-typedef HRESULT (__stdcall* wGetDeviceIdentifier)(IDirectDraw* ddObj, LPDDDEVICEIDENTIFIER2 lpddi, DWORD dwFlags);
-typedef HRESULT (__stdcall* wGetDisplayMode)(IDirectDraw* ddObj, LPDDSURFACEDESC2 lpDDSurf);
-typedef HRESULT (__stdcall* wGetFourCCCodes)(IDirectDraw* ddObj, LPDWORD lpNumCodes, LPDWORD lpCodes);
-typedef HRESULT (__stdcall* wGetGDISurface)(IDirectDraw* ddObj, LPDIRECTDRAWSURFACE7 *lplpGDIDDSurf);
-typedef HRESULT (__stdcall* wGetMonitorFrequency)(IDirectDraw* ddObj, LPDWORD lpdwFreq);
-typedef HRESULT (__stdcall* wGetScanLine)(IDirectDraw* ddObj, LPDWORD lpdwScanLine);
-typedef HRESULT (__stdcall* wGetSurfaceFromDC)(IDirectDraw* ddObj, HDC hdc, LPDIRECTDRAWSURFACE7 *lpDDS);
-typedef HRESULT (__stdcall* wGetVerticalBlankStatus)(IDirectDraw* ddObj, LPBOOL lpVB);
-typedef HRESULT (__stdcall* wInitialize)(IDirectDraw* ddObj, GUID *lpGUID);
-typedef HRESULT (__stdcall* wRestoreAllSurfaces)(IDirectDraw* ddObj);
-typedef HRESULT (__stdcall* wRestoreDisplayMode)(IDirectDraw* ddObj);
-typedef HRESULT (__stdcall* wSetCooperativeLevel)(IDirectDraw* ddObj, HWND hwnd, DWORD dwFlags);
-typedef HRESULT (__stdcall* wSetDisplayMode)(IDirectDraw* ddObj, DWORD dwWidth, DWORD dwHeight, DWORD dwBPP, DWORD dwRefresh, DWORD dwFlags);
-typedef HRESULT (__stdcall* wStartModeTest)(IDirectDraw* ddObj, LPSIZE lpModesToTest, DWORD dwNumEntries, DWORD dwFlags);
-typedef HRESULT (__stdcall* wTestCooperativeLevel)(IDirectDraw* ddObj);
-typedef HRESULT (__stdcall* wWaitForVerticalBlank)(IDirectDraw* ddObj, DWORD dwFlags, HANDLE hEvent);
+/* Same for DirectDrawSurface member functions*/
+typedef HRESULT (__stdcall* wAddAttachedSurface)(IDirectDrawSurface* surface, LPDIRECTDRAWSURFACE lpDDSurface);
+typedef HRESULT (__stdcall* wAddOverlayDirtyRect)(IDirectDrawSurface* surface, LPRECT lpRect);
+typedef HRESULT (__stdcall* wBlt)(IDirectDrawSurface* surface, LPRECT lpDestRect, LPDIRECTDRAWSURFACE lpDDSurface, LPRECT lpSrcRect, DWORD dwFlags, LPDDBLTFX lpBltFx);
+typedef HRESULT (__stdcall* wBltBatch)(IDirectDrawSurface* surface, LPDDBLTBATCH lpBltBatch, DWORD dwCount, DWORD dwFlags);
+typedef HRESULT (__stdcall* wBltFast)(IDirectDrawSurface* surface, DWORD dwX, DWORD dwY, LPDIRECTDRAWSURFACE lpDDSurface, LPRECT srcRect, DWORD dwFlags);
+typedef HRESULT (__stdcall* wChangeUniquenessValue)(IDirectDrawSurface* surface);
+typedef HRESULT (__stdcall* wDeleteAttachedSurface)(IDirectDrawSurface* surface, DWORD dwFlags, LPDIRECTDRAWSURFACE lpAttachedSurface);
+typedef HRESULT (__stdcall* wEnumAttachedSurfaces)(IDirectDrawSurface* surface, LPVOID lpContext, LPDDENUMSURFACESCALLBACK lpCallback);
+typedef HRESULT (__stdcall* wEnumOverlayZOrders)(IDirectDrawSurface* surface, DWORD dwFlags, LPVOID lpContext, LPDDENUMSURFACESCALLBACK lpCallback);
+typedef HRESULT (__stdcall* wFlip)(IDirectDrawSurface* surface, LPDIRECTDRAWSURFACE lpDDTargetOverride, DWORD dwFlags);
+typedef HRESULT (__stdcall* wFreePrivateData)(IDirectDrawSurface* surface, REFGUID guidTag);
+typedef HRESULT (__stdcall* wGetAttachedSurface)(IDirectDrawSurface* surface, LPDDSCAPS lpCaps, LPDIRECTDRAWSURFACE *lplpAttachedSurface);
+typedef HRESULT (__stdcall* wGetBltStatus)(IDirectDrawSurface* surface, DWORD dwFlags);
+typedef HRESULT (__stdcall* wGetCaps)(IDirectDrawSurface* surface, LPDDSCAPS lpDDSCaps);
+typedef HRESULT (__stdcall* wGetClipper)(IDirectDrawSurface* surface, LPDIRECTDRAWCLIPPER *lplpDDClipper);
+typedef HRESULT (__stdcall* wGetColorKey)(IDirectDrawSurface* surface, DWORD dwFlags, LPDDCOLORKEY lpDDColorKey);
+typedef HRESULT (__stdcall* wGetDC)(IDirectDrawSurface* surface, HDC *lphDC);
+typedef HRESULT (__stdcall* wGetDDInterface)(IDirectDrawSurface* surface, LPVOID *lplpDD);
+typedef HRESULT (__stdcall* wGetFlipStatus)(IDirectDrawSurface* surface, DWORD dwFlags);
+typedef HRESULT (__stdcall* wGetOverlayPosition)(IDirectDrawSurface* surface, LPLONG lplX, LPLONG lplY);
+typedef HRESULT (__stdcall* wGetPalette)(IDirectDrawSurface* surface, LPDIRECTDRAWPALETTE  *lplpDDPalette);
+typedef HRESULT (__stdcall* wGetPixelFormat)(IDirectDrawSurface* surface, LPDDPIXELFORMAT lpDDPixelFormat);
+typedef HRESULT (__stdcall* wGetPrivateData)(IDirectDrawSurface* surface, REFGUID guidTag, LPVOID  lpBuffer, LPDWORD lpcbBufferSize);
+typedef HRESULT (__stdcall* wGetSurfaceDesc)(IDirectDrawSurface* surface, LPDDSURFACEDESC lpDDSurfaceDesc);
+typedef HRESULT (__stdcall* wGetUniquenessValue)(IDirectDrawSurface* surface, LPDWORD lpValue);
+typedef HRESULT (__stdcall* wInitialize)(IDirectDrawSurface* surface, LPDIRECTDRAW lpDD, LPDDSURFACEDESC lpDDSurfaceDesc);
+typedef HRESULT (__stdcall* wIsLost)(IDirectDrawSurface* surface);
+typedef HRESULT (__stdcall* wLock)(IDirectDrawSurface* surface, LPRECT lpDestRect, LPDDSURFACEDESC lpDDSurfaceDesc, DWORD  dwFlags, HANDLE hEvent);
+typedef HRESULT (__stdcall* wPageLock)(IDirectDrawSurface* surface, DWORD dwFlags);
+typedef HRESULT (__stdcall* wPageUnlock)(IDirectDrawSurface* surface, DWORD dwFlags);
+typedef HRESULT (__stdcall* wReleaseDC)(IDirectDrawSurface* surface, HDC hDC);
+typedef HRESULT (__stdcall* wRestore)(IDirectDrawSurface* surface);
+typedef HRESULT (__stdcall* wSetClipper)(IDirectDrawSurface* surface, LPDIRECTDRAWCLIPPER lpDDClipper);
+typedef HRESULT (__stdcall* wSetColorKey)(IDirectDrawSurface* surface, DWORD dwFlags, LPDDCOLORKEY lpDDColorKey);
+typedef HRESULT (__stdcall* wSetOverlayPosition)(IDirectDrawSurface* surface, LONG lX, LONG lY);
+typedef HRESULT (__stdcall* wSetPalette)(IDirectDrawSurface* surface, LPDIRECTDRAWPALETTE lpDDPalette);
+typedef HRESULT (__stdcall* wSetPrivateData)(IDirectDrawSurface* surface, REFGUID guidTag, LPVOID  lpData, DWORD cbSize, DWORD dwFlags);
+typedef HRESULT (__stdcall* wSetSurfaceDesc)(IDirectDrawSurface* surface, LPDDSURFACEDESC lpDDsd2, DWORD dwFlags);
+typedef HRESULT (__stdcall* wUnlock)(IDirectDrawSurface* surface, LPVOID lpRect);
+typedef HRESULT (__stdcall* wUpdateOverlay)(IDirectDrawSurface* surface, LPRECT lpSrcRect, LPDIRECTDRAWSURFACE lpDDDestSurface, LPRECT lpDestRect, DWORD dwFlags, LPDDOVERLAYFX lpDDOverlayFx);
+typedef HRESULT (__stdcall* wUpdateOverlayDisplay)(IDirectDrawSurface* surface, DWORD dwFlags);
+typedef HRESULT (__stdcall* wUpdateOverlayZOrder)(IDirectDrawSurface* surface, DWORD dwFlags, LPDIRECTDRAWSURFACE lpDDSReference);
 
 /*
 Declare pointers to hooked functions
@@ -80,11 +95,56 @@ wDDInternalLock					pDDInternalLock;
 wDDInternalUnlock				pDDInternalUnlock;
 wDSoundHelp						pDSoundHelp;
 wGetDDSurfaceLocal				pGetDDSurfaceLocal;
-wGetSurfaceFromDC_export				pGetSurfaceFromDC;
+wGetSurfaceFromDC_export		pGetSurfaceFromDC;
 wRegisterSpecialCase			pRegisterSpecialCase;
+
+/* Same for DirectDrawSurface member functions*/
+wAddAttachedSurface				pAddAttachedSurface;
+wAddOverlayDirtyRect			pAddOverlayDirtyRect;
+wBlt							pBlt;
+wBltBatch						pBltBatch;
+wBltFast						pBltFast;
+wChangeUniquenessValue			pChangeUniquenessValue;
+wDeleteAttachedSurface			pDeleteAttachedSurface;
+wEnumAttachedSurfaces			pEnumAttachedSurfaces;
+wEnumOverlayZOrders				pEnumOverlayZOrders;
+wFlip							pFlip;
+wFreePrivateData				pFreePrivateData;
+wGetAttachedSurface				pGetAttachedSurface;
+wGetBltStatus					pGetBltStatus;
+wGetCaps						pGetCaps;
+wGetClipper						pGetClipper;
+wGetColorKey					pGetColorKey;
+wGetDC							pGetDC;
+wGetDDInterface					pGetDDInterface;
+wGetFlipStatus					pGetFlipStatus;
+wGetOverlayPosition				pGetOverlayPosition;
+wGetPalette						pGetPalette;
+wGetPixelFormat					pGetPixelFormat;
+wGetPrivateData					pGetPrivateData;
+wGetSurfaceDesc					pGetSurfaceDesc;
+wGetUniquenessValue				pGetUniquenessValue;
+wInitialize						pInitialize;
+wIsLost							pIsLost;
+wLock							pLock;
+wPageLock						pPageLock;
+wPageUnlock						pPageUnlock;
+wReleaseDC						pReleaseDC;
+wRestore						pRestore;
+wSetClipper						pSetClipper;
+wSetColorKey					pSetColorKey;
+wSetOverlayPosition				pSetOverlayPosition;
+wSetPalette						pSetPalette;
+wSetPrivateData					pSetPrivateData;
+wSetSurfaceDesc					pSetSurfaceDesc;
+wUnlock							pUnlock;
+wUpdateOverlay					pUpdateOverlay;
+wUpdateOverlayDisplay			pUpdateOverlayDisplay;
+wUpdateOverlayZOrder			pUpdateOverlayZOrder;
 
 /*
 Declare other global stuff
 */
 
 HMODULE sysDDraw;
+boolean initializedSurfaceHooks = false;
